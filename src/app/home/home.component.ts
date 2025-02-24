e58451a8f9caca5e14593bd8fb864c79ca7243de
Dimitrije Jelic <dimitrije.jelic00@gmail.com>
2025-02-24 13:44:58 +0100

    * Implemented flight model

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,23 @@
 import { Component } from '@angular/core';
 import {FlightService} from '../../services/flight.service';
-import {JsonPipe, NgIf} from '@angular/common';
+import {NgForOf, NgIf} from '@angular/common';
 import {AxiosError} from 'axios';
+import {FlightModel} from '../../models/flight.model';
 
 @Component({
   selector: 'app-home',
-  imports: [JsonPipe, NgIf],
+  imports: [NgIf, NgForOf],
   templateUrl: './home.component.html',
   standalone: true,
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  public flights: any[] | null = null;
+  public flights: FlightModel[] | null = null;
   public error: string | null = null;
 
   constructor() {
     FlightService.findAll().then((response) => {
-      this.flights = response.data;
+      this.flights = response.data.content;
     }).catch((error: AxiosError) => this.error = `${error.code}: ${error.message}`);
   }
 }
