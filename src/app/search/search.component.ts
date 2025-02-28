67f107dd87373d5e11afa1b81faca99c841d242e
Dimitrije Jelic <dimitrije.jelic00@gmail.com>
2025-02-28 19:28:25 +0100

    * Created search component

---
+++ src/app/search/search.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {MatTableModule} from '@angular/material/table';
+import {FlightModel} from '../../models/flight.model';
+import {DatePipe, NgIf} from '@angular/common';
+import {FlightService} from '../../services/flight.service';
+import {MatButton} from '@angular/material/button';
+
+@Component({
+  selector: 'app-search',
+  imports: [MatTableModule, NgIf, DatePipe, MatButton],
+  templateUrl: './search.component.html',
+  standalone: true,
+  styleUrl: './search.component.css'
+})
+export class SearchComponent {
+  displayedColumns: string[] = ['id', 'destination', 'flightNumber', 'scheduledAt', 'actions'];
+  dataSource: FlightModel[] | null = null;
+
+  public constructor() {
+    FlightService.findAll(0, 8).then(response => this.dataSource = response.data.content);
+  }
+}
